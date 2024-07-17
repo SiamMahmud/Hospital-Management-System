7edec353fe80424a8e07085234a003b9c0d88bd6
SiamMahmud <rakibsiam421@gmail.com>
2024-07-18 03:45:00 +0800

    Added login navigation and data binding SM-1000

--- app/src/main/java/com/example/hospitalmanagement/presentation/MainActivity.kt
+++ app/src/main/java/com/example/hospitalmanagement/presentation/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.hospitalmanagement.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hospitalmanagement.R
+import com.example.hospitalmanagement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
     }
+    companion object {
+        fun getLaunchIntent(context: Context) = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        }
+    }
+
 }
