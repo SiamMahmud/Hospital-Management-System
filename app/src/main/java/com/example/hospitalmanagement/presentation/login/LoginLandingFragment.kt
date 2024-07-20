3ca4ce0b3db1aae521b930dd89b0d1ef61677f52
SiamMahmud <rakibsiam421@gmail.com>
2024-07-21 03:52:45 +0800

    Desinged Login Landing Fragment SM-1002

--- app/src/main/java/com/example/hospitalmanagement/presentation/login/LoginLandingFragment.kt
+++ app/src/main/java/com/example/hospitalmanagement/presentation/login/LoginLandingFragment.kt
@@ -5,13 +5,134 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.viewpager2.widget.ViewPager2
 import com.example.hospitalmanagement.R
+import com.example.hospitalmanagement.databinding.FragmentLoginLandingBinding
+
 class LoginLandingFragment : Fragment() {
+
+    val actionLogin =
+        Navigation.createNavigateOnClickListener(R.id.action_loginLandingFragment_to_loginInputFragment)
+    val actionSignUp =
+        Navigation.createNavigateOnClickListener(R.id.action_loginLandingFragment_to_loginCreateFragment)
+
+    private lateinit var binding: FragmentLoginLandingBinding
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_landing, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_login_landing, container, false)
+        binding.model = this
+
+        val pageOne = LayoutInflater.from(activity).inflate(R.layout.login_slider_page_one, null)
+        val pageTwo = LayoutInflater.from(activity).inflate(R.layout.login_slider_page_two, null)
+        val pageThree =
+            LayoutInflater.from(activity).inflate(R.layout.login_slider_page_three, null)
+
+        val views = listOf(pageOne, pageTwo, pageThree)
+        val adapter = ViewPagerAdapter(views)
+
+        binding.sliderViewPager.adapter = adapter
+        binding.sliderViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                changeColor()
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels)
+            }
+
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                super.onPageScrollStateChanged(state)
+                changeColor()
+            }
+
+        })
+
+
+        return binding.root
     }
+
+    private fun changeColor() {
+        val context = requireContext()
+        when (binding.sliderViewPager.currentItem) {
+            0 -> {
+                binding.dotOneIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.red_500
+                    )
+                )
+                binding.dotTwoIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+                binding.dotThreeIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+            }
+
+            1 -> {
+                binding.dotOneIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+                binding.dotTwoIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.red_500
+                    )
+                )
+                binding.dotThreeIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+            }
+
+            2 -> {
+                binding.dotOneIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+                binding.dotTwoIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.slidePage
+                    )
+                )
+                binding.dotThreeIv.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.red_500
+                    )
+                )
+            }
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/hospitalmanagement/presentation/login/ViewPagerAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.hospitalmanagement.presentation.login
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ListView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hospitalmanagement.R
+
+class ViewPagerAdapter(private val views: List<View>) :
+    RecyclerView.Adapter<ViewPagerAdapter.ViewPageViewHolder>() {
+    class ViewPageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val container: FrameLayout = itemView.findViewById(R.id.view_container)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPageViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.login_landing_items, parent, false)
+        return ViewPageViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return views.size
+    }
+
+    override fun onBindViewHolder(holder: ViewPageViewHolder, position: Int) {
+        holder.container.addView(views[position])
+    }
+}
