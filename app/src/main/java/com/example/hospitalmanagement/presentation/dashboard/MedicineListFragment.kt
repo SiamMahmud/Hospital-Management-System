8766fc37770ec747565c7692ea7107c85ab01b9a
SiamMahmud <rakibsiam421@gmail.com>
2024-07-30 22:30:03 +0800

    Designed Add Medicine Fragment SM-1017

--- app/src/main/java/com/example/hospitalmanagement/presentation/dashboard/MedicineListFragment.kt
+++ app/src/main/java/com/example/hospitalmanagement/presentation/dashboard/MedicineListFragment.kt
@@ -6,18 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hospitalmanagement.R
 import com.example.hospitalmanagement.databinding.FragmentMedicineBinding
 import com.example.hospitalmanagement.presentation.adapter.AdminViewMedicineListAdapter
 import com.example.hospitalmanagement.presentation.model.MedicineInfo
+import com.example.hospitalmanagement.presentation.util.HMSActivityUtil
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MedicineListFragment : Fragment() {
+    val actionAddMedicineList = Navigation.createNavigateOnClickListener(R.id.action_medicineListFragment_to_addMedicineFragment)
     private lateinit var binding : FragmentMedicineBinding
     private lateinit var adapter: AdminViewMedicineListAdapter
     private lateinit var medicineArray: ArrayList<MedicineInfo>
     lateinit var arrayMediList: Array<String>
+    @Inject
+    lateinit var activityUtil: HMSActivityUtil
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,6 +39,7 @@ class MedicineListFragment : Fragment() {
         medicineArray = arrayListOf()
         adapter = AdminViewMedicineListAdapter(medicineArray)
         binding.medicineListRecycle.adapter = adapter
+        activityUtil.hideBottomNavigation(true)
         binding.backIv.setOnClickListener {
             findNavController().popBackStack()
         }
